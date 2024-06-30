7d3d2541bb2a4b36b06ddd4965a2daab17787f52
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2024-06-29 22:27:08 -0400

    cambios clase miercoles
    cambios de la clase miercoles 26

--- src/pages/Pagina1.tsx
+++ src/pages/Pagina1.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-function Pagina1() {
+export const Pagina1=()=> {
+    const [nombre, setNombre] = useState("Joselito")
+    const [apellido, setApellido] = useState("Rojas")
+    const [eNombre, setENombre]= useState("")
+    const validarNombre = (valor:string) =>{
+        if(valor.length > 4){
+            setENombre("");
+        }else{
+            setENombre("Debes ingresar al menos 4 caracteres")
+        }
+        setNombre(valor);
+    }
+    const registrar =()=>{
+        console.log(nombre+" "+apellido)
+        alert("Registrate:"+nombre+" "+apellido)
+    }
     return (
-        <div>Pagina1</div>
+        <>
+        <h1>Bienvenido {nombre},{apellido}</h1>
+        <Form>
+        <Form.Group>
+        <Form.Label>Nombre:</Form.Label>
+        <Form.Control type='text' placeholder='Ingresa tu nombre'
+        onChange={(e) =>{setNombre(e.currentTarget.value)}}/>
+        <Form.Text>
+            {eNombre}
+        </Form.Text>    
+        </Form.Group>
+        <Form.Group>
+        <Form.Label>Apellido</Form.Label>
+        <Form.Control type='text' placeholder='Ingresar tu apellido'
+        onChange={(e) =>{setApellido(e.currentTarget.value)}}/>  
+        </Form.Group>
+        <Button variant='primary' onClick={registrar}>Registrar</Button>
+        </Form>
+
+
+
+
+        <form>
+            <label>Nombre:</label>
+            <input type='text' placeholder='Ingrese nombre'/>
+            <label>Apellido:</label>
+            <input type='text' placeholder='Ingrese Apellido'/>
+            <button type='button'>Registrar</button>
+        </form>
+        </>
     )
 }
+
+
+
 
 export default Pagina1
---
+++ src/pages/Pagina2.tsx
@@ -0,0 +1,43 @@
+import React, {useEffect, useState} from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+export const Pagina2=()=>{
+    const [n1,setN1] = useState(0)
+    const [n2,setN2] = useState(0)
+    const [resultado, setResultado] =useState(0)
+
+    const actualizarN1 = (valor:number)=>{
+        setN1(valor)
+        sumar()
+    }
+    const actualizarN2 =(valor:number)=>{
+        setN2(valor)
+        sumar()
+    }
+    const sumar =()=>{
+        const t = n1 +n2
+        setResultado
+    }
+    useEffect(()=>{
+        sumar()
+    },[n1,n2])
+
+    return(
+        <>
+        <Form>
+        <Form.Group>
+        <Form.Label>N1:</Form.Label>
+        <Form.Control type='text' placeholder='Ingresaun numero'
+        onChange={(e) =>{setN1(parseInt(e.currentTarget.value))}}/>   
+        </Form.Group>
+        <Form.Group>
+        <Form.Label>N2</Form.Label>
+        <Form.Control type='text' placeholder='Ingresar un numero'
+        onChange={(e) =>{setN2(parseInt(e.currentTarget.value))}}/>  
+        </Form.Group>
+        <Form.Label>Resultado</Form.Label>
+        </Form>
+        </>
+    )
+}
