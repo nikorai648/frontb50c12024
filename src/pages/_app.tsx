b4dc95e4c7410d968e4f5fbb48a2bde774c689f6
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2024-07-01 17:48:45 -0400

    cambio faltante

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-
+import React, {useState} from 'react'
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
